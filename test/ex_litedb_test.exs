9cd9ff3b96164f2d093a2623035a72142eab0ba1
Roman Heinrich <roman.heinrich@gmail.com>
2021-10-16 22:37:25 +0200

    Add basic sanity tests

--- test/ex_litedb_test.exs
+++ test/ex_litedb_test.exs
@@ -1,4 +1,12 @@
 defmodule ExLitedbTest do
   use ExUnit.Case
   doctest ExLitedb
+
+  describe "path_for" do
+    test "works" do
+      ExLitedb.extensions() |> Enum.map(fn(ext)->
+        assert ExLitedb.path_for(ext) |> IO.inspect()
+      end)
+    end
+  end
 end
